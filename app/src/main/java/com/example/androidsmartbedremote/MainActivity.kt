c8f7787c2fc340c674f0587f3818745afa8c1ad5
Mathew Szymanowski <m96szy@gmail.com>
2022-08-30 13:00:25 -0400

    implemented navigation and added bluetooth fragment

--- app/src/main/java/com/example/androidsmartbedremote/MainActivity.kt
+++ app/src/main/java/com/example/androidsmartbedremote/MainActivity.kt
@@ -5,27 +5,45 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.androidsmartbedremote.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+    private lateinit var overflowMenu: Menu
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
+        overflowMenu = menu
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.bluetooth_connection -> {
-                Toast.makeText(this, "bluetooth", Toast.LENGTH_SHORT).show()
+                navController.navigate(R.id.action_remoteFragment_to_bluetoothFragment)
+                hideOverflowMenu()
                 true
             }
             R.id.settings -> {
@@ -34,5 +52,15 @@ class MainActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    fun hideOverflowMenu() {
+        overflowMenu.findItem(R.id.bluetooth_connection).setVisible(false)
+        overflowMenu.findItem(R.id.settings).setVisible(false)
     }
 }
