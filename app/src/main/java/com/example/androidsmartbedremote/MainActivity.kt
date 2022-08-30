4c0847ea6cd5a537bace1c3678634b226075cdaa
Mathew Szymanowski <m96szy@gmail.com>
2022-08-29 21:04:21 -0400

    added menu worth basic implementation and updated gradle

--- app/src/main/java/com/example/androidsmartbedremote/MainActivity.kt
+++ app/src/main/java/com/example/androidsmartbedremote/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.androidsmartbedremote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.androidsmartbedremote.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.bluetooth_connection -> {
+                Toast.makeText(this, "bluetooth", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.settings -> {
+                Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
